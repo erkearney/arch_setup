cbcc89f629c52e094473058e93b89ec0b9977bc6
eric <erkearney>
2020-12-26 18:38:40 -0700

    Organized root script into functions

--- root_arch_setup.sh
+++ root_arch_setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+systembeepoff() { dialog "Get rid of the worse error beep sound I've ever heard..." 10 50
+    rmmod pcspkr
+    echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
+
+updateconfigfiles() { \
+    echo "options snd_hda_intel dmic_detect=0" > /etc/modprobe.d/disable_dmic.conf ;}
+
 pacman -Syuu
-pacman -S vim yay base-devel xclip xorg-xinput i3-gaps dmenu
-echo "options snd_hda_intel dmic_detect=0" > /etc/modprobe.d/disable_dmic.conf
-rmmod pcspkr
-echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+pacman -S vim yay base-devel xclip xorg-xinput i3-gaps dmenu i3status
+systembeepoff
+updateconfigfiles
