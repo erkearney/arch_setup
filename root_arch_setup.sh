38a13acf4dd5f0514a3c13b861a1211f614b351b
e <erickearney@protonmail.com>
2021-02-19 15:04:33 -0700

    Removed redundencies

--- root_arch_setup.sh
+++ root_arch_setup.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-systembeepoff() { dialog "Get rid of the worse error beep sound I've ever heard..." 10 50
+systembeepoff() { 
     rmmod pcspkr
     echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
 
-fixsoundissue() { \
-    echo "options snd_hda_intel dmic_detect=0" > /etc/modprobe.d/disable_dmic.conf ;}
-
-updateinstallpkgs() {\
+updateinstallpkgs() {
     pacman -Syuu
-    pacman -Syy vim nextcloutd-cleint base-devel xclip xorg-xinput spectacle \
-                i3-gaps dmenu i3status xterm ttf-linux-libertine sof-firmware \
-                ttf-inconsolata zathura feh xwallpaper nmap xcompmgr ;}
-
-setupgithub() {\
-    read -r -p "Enter your github email: " GITHUBEMAIL
-    git config --global user.email "$GITHUBEMAIL"
-    ssh-keygen -t rsa -b 4096 -C "$GITHUBEMAIL"
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/id_rsa
-    xclip -sel clip ~/.ssh/id_rsa.pub
-    firefox https://github.com/settings/keys 
-    read -p "Go enter your new SSH key, Press enter to continue" ;}
+    pacman -Syy vim \
+		nextcloud-client \
+		base-devel \
+		xclip \
+		xorg-xinput \
+		spectacle \
+                i3-gaps \
+		dmenu \
+		i3status \
+		xterm \
+		ttf-linux-libertine \
+		sof-firmware \
+                ttf-inconsolata \
+		zathura \
+		feh \
+		xwallpaper \
+		nmap \
+		xcompmgr \
+		alsa-utils ;}
 
 
 updateinstallpkgs
 systembeepoff
-fixsoundissue
